b343f65d36520d43ba41bbe7d54c96c4a76f5f20
choi4733 <choiheemin1324@gmail.com>
2026-03-30 17:33:34 +0900

    메세지

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./components/Header";
+import Slider from "./components/Slider";
+import Image from "./components/Image";
+import ImageText from "./components/ImageText";
+import Card from "./components/Card";
+import Text from "./components/Text";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Slider />
+      <Image />
+      <ImageText />
+      <Card />
+      <Text />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
