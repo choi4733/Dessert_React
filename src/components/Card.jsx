78006ee79fdbdef8f89c6af882e3e0115284ae35
choi4733 <choiheemin1324@gmail.com>
2026-04-01 15:52:58 +0900

    자스 gsap aos 사용

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { cardData } from "../data/cardData";
-const Card = (props) => {
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const Card = ({ title, element }) => {
+  AOS.init();
+
   return (
     <section id="cardType" className="section">
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
       <p className="element">
         투썸플레이스는 프리미엄 커피 & 디저트 카페의 리딩 브랜드로서 디저트 카페 문화의 차별화된 제품과 서비스를
         선보일것입니다.
       </p>
       <div className="card_inner container">
-        {cardData.map((card) => (
-          <article className="card" key={card.id}>
+        {cardData.map((card, index) => (
+          <article className="card" key={card.id} data-aos="flip-left" data-aos-delay={index * 150}>
             <figure className="card_header">
               <img src={card.img} alt="디저트이미지" />
             </figure>
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { imageData } from "../data/imageData";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 const Image = (props) => {
+  const imgInnerRef = useRef(null);
+  const imgRef = useRef([]);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.fromTo(
+      imgRef.current,
+      {
+        scale: 0.5,
+        opacity: 0,
+      },
+      {
+        scale: 1,
+        opacity: 1,
+        ease: "bounce.inOut",
+
+        scrollTrigger: {
+          trigger: imgInnerRef.current,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+        },
+      },
+    );
+  }, []);
+
   return (
     <section id="imageType" className="section">
       <h1 className="blind">{props.title}</h1>
       <h2>프리미어 케이크와 디저트를 만나보세요</h2>
       <p>유러피언 디저트의 본질을 추구한 고급 원재료와 차별화된 레시피</p>
-      <div className="image_inner container">
-        {imageData.map((img) => (
-          <article className={`image ${img.className}`} key={img.id}>
+      <div className="image_inner container" ref={imgInnerRef}>
+        {imageData.map((img, index) => (
+          <article className={`image ${img.className}`} key={img.id} ref={(el) => (imgRef.current[index] = el)}>
             <h3 className="image_title">{img.title}</h3>
             <p className="image_desc">{img.desc}</p>
             <p className={`image_btn ${img.btn}`}>자세히 보기</p>
